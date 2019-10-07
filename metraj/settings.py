2bc650f1a2d07517e59436615fe14a1521ec994f
Mojtaba Mohammadi <mo71.mohammadi@gmail.com>
2019-10-07 02:46:13 -0400

    static_root

--- metraj/settings.py
+++ metraj/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
 ]
