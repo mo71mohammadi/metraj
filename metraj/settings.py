15325e2bf0582d5262f8aa52258b089b4e2d35d0
Mojtaba Mohammadi <mo71.mohammadi@gmail.com>
2019-10-07 02:20:31 -0400

    static_root

--- metraj/settings.py
+++ metraj/settings.py
@@ -70,18 +70,15 @@ TEMPLATES = [
     },
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/home/metraj/metraj_data_django/static'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
-# ]
-# MEDIA_URL = '/media'
-STATICFILES_DIRS = [BASE_DIR+"/assets"]
-STATIC_ROOT = '/home/metraj/metraj_data_django/static'
-# MEDIA_ROOT = '/home/cityart2/public_html/media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/home/metraj/metraj_data_django/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
+]
+# # MEDIA_URL = '/media'
+# STATICFILES_DIRS = [BASE_DIR+"/assets"]
+# STATIC_ROOT = '/home/metraj/metraj_data_django/static'
+# # MEDIA_ROOT = '/home/cityart2/public_html/media'
 
 
 WSGI_APPLICATION = 'metraj.wsgi.application'
