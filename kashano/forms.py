a07f382dad09cf9556d56f6719e0cb46231e3f91
Mojtaba Mohammadi <mo71.mohammadi@gmail.com>
2019-10-15 06:32:57 -0400

    change database

--- kashano/forms.py
+++ kashano/forms.py
@@ -26,3 +26,94 @@ class EstateForm(forms.ModelForm):
         # fields = '__all__'
         exclude = ['download_status', 'download_time', 'delete_status', 'name', 'owner_phone', 'owner_phone2']
     field_order = ['elead_id', 'est_type', 'area_id']
+
+def change_data(new_list):
+    kashano_new_list = []
+    for obj in new_list:
+        newObj = {}
+        newObj['id'] = obj['id']
+        newObj['estate_type_id'] = obj['est_type']
+        del obj['est_type']
+        newObj['region_id'] = obj['area_id']
+        del obj['area_id']
+        newObj['transaction_type_id'] = obj['deal_type']
+        del obj['deal_type']
+        newObj['estate_id'] = obj['elead_id']
+        del obj['elead_id']
+        newObj['name'] = obj['name']
+        del obj['owner_name']
+        del obj['name']
+        if not obj['phone2']:
+            obj['phone2'] = ''
+        Phone = re.search(r'''^[9]\d{9}|^[0]\d{10}''', obj['phone'])
+        Phone2 = re.search(r'''^[9]\d{9}|^[0]\d{10}''', obj['phone2'])
+        if Phone:
+            newObj['mobile'] = Phone[0]
+            newObj['phone'] = obj['phone2']
+        elif Phone2:
+            newObj['mobile'] = Phone2[0]
+            newObj['phone'] = obj['phone']
+        else:
+            newObj['mobile'] = ''
+            newObj['phone'] = ''
+        del obj['phone']
+        del obj['phone2']
+        del obj['owner_phone']
+        del obj['owner_phone2']
+        newObj['address'] = obj['est_address']
+        del obj['est_address']
+        newObj['sale_price'] = obj['price']
+        del obj['price']
+        newObj['mortgage_price'] = obj['price_rahn']
+        del obj['price_rahn']
+        newObj['rent_price'] = obj['price_rent']
+        del obj['price_rent']
+        newObj['description'] = obj['dscr']
+        del obj['dscr']
+        newObj['area'] = obj['size_subs']
+        del obj['size_subs']
+        newObj['age'] = obj['age']
+        del obj['age']
+        newObj['front_length'] = ''
+        newObj['direction'] = obj['direction']
+        del obj['direction']
+        newObj['rooms_count'] = obj['rooms']
+        del obj['rooms']
+        newObj['floors_count'] = obj['floors']
+        del obj['floors']
+        newObj['units_count'] = ''
+        newObj['floor'] = obj['floor']
+        del obj['floor']
+        newObj['total_units_count'] = ''
+        newObj['cover'] = obj['flooring']
+        del obj['flooring']
+        newObj['frontage'] = obj['view_material']
+        del obj['view_material']
+        newObj['wc'] = obj['toilet']
+        del obj['toilet']
+        newObj['kitchen'] = obj['cabinet']
+        del obj['cabinet']
+        newObj['document'] = obj['sanad_stat']
+        del obj['sanad_stat']
+        newObj['habitation'] = obj['fill_stat']
+        del obj['fill_stat']
+        newObj['map'] = str(obj['addr_latitude']) + ', ' + str(obj['addr_longitude'])
+        del obj['addr_latitude']
+        del obj['addr_longitude']
+        newObj['has_modify'] = obj['eslahi']
+        del obj['eslahi']
+        newObj['has_manufacturing_license'] = obj['javaz_sakht']
+        del obj['javaz_sakht']
+        newObj['urban_position'] = ''
+        newObj['has_endofwork'] = ''
+        newObj['created_at'] = obj['ctime']
+        del obj['ctime']
+        newObj['updated_at'] = obj['utime']
+        del obj['utime']
+        newObj['area_kashano'] = obj['area']
+        del obj['area']
+
+        newObj.update(obj)
+        kashano_new_list.append(newObj)
+    return kashano_new_list
+
